4c39465eb89021107811e3e4c5be75f0dd22674c
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-03 23:20:20 -0400

    Push main.js error catching and version checkers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-const nodeMajorVersion = parseInt(process.versions.node.split('.')[0], 10);
-if (nodeMajorVersion < 8) {
-  console.error('Please install Node.js version 8 or above.');
-  //throw new SystemError('The current version of Node.js that you have is unsupported. Please install version 8 or above.');
-  process.kill(process.pid, 'SIGKILL');
-}
+const Discord = require('discord.js');
+
+const client = new Discord.Client({
+  fetchAllMembers: true,
+  disableEveryone: true
+});
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready.`);
+});
+
